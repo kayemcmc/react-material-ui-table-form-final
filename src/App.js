342116c5e70f2cf51b477a920d7f71ad6bd27c9f
Karen Posada <k100posada@gmail.com>
2017-11-24 23:14:04 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./Form";
+import Table from "./Table/Table";
+
+injectTapEventPlugin();
+
+class App extends Component {
+  state = {
+    data: [],
+  };
+
+
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div className="App ">
+          <Form onSubmit={submission => this.setState({
+            data: [...this.state.data, submission]
+          })} />
+          <Table
+            data = {this.state.data}
+            header={[
+              {
+                name: "First name",
+                prop: 'firstName',
+              },
+              {
+                name: "Last name",
+                prop: 'lastName',
+              },
+              {
+                name: "Username",
+                prop: 'username',
+              },
+              {
+                name: "Email",
+                prop: 'email',
+              },
+              {
+                name: "Password",
+                prop: 'password',
+              },
+          ]}/>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
